e35ed3ee04911cd6ae3c06ab5ed909224286c94f
soniya1987 <vksoniya68@gmail.com>
2023-08-09 14:56:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeWithFloatingInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 10.01;
+            Console.WriteLine(string.Format("the number {0} is{1}a Palindrome", number, (IsPalindrome(number) ? " " : " not ")));
+            number = 10.012;
+            Console.WriteLine(string.Format("the number {0} is{1}a Palindrome", number, (IsPalindrome(number) ? " " : " not ")));
+            number = 1001;
+            Console.WriteLine(string.Format("the number {0} is{1}a Palindrome", number, (IsPalindrome(number) ? " " : " not ")));
+            number = -1001;
+            Console.WriteLine(string.Format("the number {0} is{1}a Palindrome", number, (IsPalindrome(number) ? " " : " not ")));
+            Console.Read();
+        }
+
+        static bool IsPalindrome(double num)
+        {
+            string actual = num.ToString().Replace("-", "");
+            for (int i = actual.Length - 1; i >= actual.Length / 2; i--)
+            {
+                //Console.WriteLine("The Character is " + actual[i] + ", " + actual[(actual.Length - i) - 1]);
+                if (actual[i] != actual[(actual.Length - i) - 1])
+                    return false;
+                else
+                    continue;
+            }
+            return true;
+        }
+    }
+}
